8cedbe5e3c4e21e6eea4397a82419eda180b020c
sharonWU0505 <astrumstella048@gmail.com>
2018-04-24 13:09:43 +0800

    Test push message again

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
-from linebot.exceptions import InvalidSignatureError
+from linebot.exceptions import InvalidSignatureError, LineBotApiError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage, ImageSendMessage
 import os
 
@@ -33,12 +33,26 @@ def callback():
     return 'OK'
 
 
+# get user name
+try:
+    profile = line_bot_api.get_profile(user_id)
+    print(profile.display_name)
+    print(profile.user_id)
+    print(profile.picture_url)
+    print(profile.status_message)
+    name = profile.display_name
+except LineBotApiError as e:
+    name = 'friend'
+
+
+# push message to one user
+hello_message = TextSendMessage(text='Dear {}, I will talk about myself to let \
+                                you know more about me.'.format(name))
+line_bot_api.push_message(user_id, hello_message)
+
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    # push message to one user
-    hello_message = TextSendMessage(text='What\'s your name?')
-    line_bot_api.push_message(user_id, hello_message)
-
     message = TextSendMessage(text=event.message.text)
     line_bot_api.reply_message(event.reply_token, message)
 
