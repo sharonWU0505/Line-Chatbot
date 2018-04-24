f4f8aedf07ce5bbfb8e297bf634e07f6f7fec35a
sharonWU0505 <astrumstella048@gmail.com>
2018-04-24 22:39:23 +0800

    Fix img problems

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ def handle_message(event):
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=content))
         return 0
 
-    elif 'research' in text or '5G' in text or 'network' in text:
+    if 'research' in text or '5G' in text or 'network' in text:
         res_content = '5G network is known for its centralized structure, which makes it more flexible to meet \
                        heterogeneous demand. However, the way to allocate resource remains a problem.'
         objectives = 'So I aim to design suitable allocation scheme for different scenarios.'
@@ -60,7 +60,7 @@ def handle_message(event):
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=content))
         return 0
 
-    elif 'intern' in text or 'internship' in text:
+    if 'intern' in text or 'internship' in text:
         message = TemplateSendMessage(
             alt_text='Internship in Gliacloud',
             template=ButtonsTemplate(
@@ -86,36 +86,36 @@ def handle_message(event):
         line_bot_api.reply_message(event.reply_token, message)
         return 0
 
-    elif 'gliacloud' in text:
+    if 'gliacloud' in text:
         content = 'Gliacloud is a startup developing AI video creation platform. You could find more information on their website:\nhttps://www.gliacloud.com/'
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=content))
         return 0
 
-    elif 'achievement' in text:
+    if 'achievement' in text:
         content = '1. Developed and maintained UI of the AI Video Creation Platform and the official website.\n2. Supported and maintained quality control system based on user feedback.\n3. Wrote and executed unit tests and system tests for core products.\n4. Provided sales related support, including demo video production and business model analysis.'
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=content))
         return 0
 
-    elif 'reflection' in text:
+    if 'reflection' in text:
         content = 'I\'m very happy and also thankful for having the chance to apply what I learned to the real world.'
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=content))
         return 0
 
-    elif 'project' in text:
+    if 'project' in text:
         message = TemplateSendMessage(
-            alt_text='My Works',
+            alt_text='I\'ve made two projects',
             template=ButtonsTemplate(
                 thumbnail_image_url='https://i.imgur.com/nhX3VEW.jpg',
-                title='My Works',
-                text='Pleas select',
+                title='I\'ve made two projects',
+                text='Select one to know more',
                 actions=[
                     MessageTemplateAction(
                         label='UP YOUNG System',
-                        text='UP YOUNG System'
+                        text='UP YOUNG Maintenance Staff System'
                     ),
                     MessageTemplateAction(
                         label='Trisoap System',
-                        text='Trisoap System'
+                        text='Trisoap Sales System'
                     ),
                 ]
             )
@@ -123,19 +123,19 @@ def handle_message(event):
         line_bot_api.reply_message(event.reply_token, message)
         return 0
 
-    elif 'up young' in text:
-        content = '[UP YOUNG Maintenance Staff System]\nUP YOUNG is a washing machine dealer with many customers.\n\nWhat we do is to digitize and optimize the workflow of their maintenance staff who have to check whether each washing machine works well.\n\nWe developed optimized algorithm for scheduling and traveling route, in order to suggest best schedule for maintenance staff members.'
+    if 'up young' in text:
+        content = '[UP YOUNG Maintenance Staff System]\n\nUP YOUNG is a washing machine dealer with many customers.\n\nWhat we do is to digitize and optimize the workflow of their maintenance staff who have to check whether each washing machine works well.\n\nWe developed optimized algorithm for scheduling and traveling route, in order to suggest best schedule for maintenance staff members.'
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=content))
         return 0
 
-    elif 'trisoap' in text:
-        tri_content = '[Trisoap Sales System]\nTrisoap is a brand dedicated to sale soap made by mentally retarded children.\n\nWe collected their system demand by face-to-face interview and designed an appropriate system.\nFinally, we developed the online purchasing system, including connecting with third-party payment service and build admin interface for managers.'
+    if 'trisoap' in text:
+        tri_content = '[Trisoap Sales System]\n\nTrisoap is a brand dedicated to sale soap made by mentally retarded children.\n\nWe collected their system demand by face-to-face interview and designed an appropriate system.\nFinally, we developed the online purchasing system, including connecting with third-party payment service and build admin interface for managers.'
         link = 'You can see the website here:\nhttps://www.trisoap.com.tw/'
         content = '{}\n\n{}'.format(tri_content, link)
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=content))
         return 0
 
-    elif 'this might be something interesting in your school life' in text:
+    if 'this might be something interesting in your school life' in text:
         message = TemplateSendMessage(
             alt_text='Extracurricular Activities',
             template=ButtonsTemplate(
@@ -157,21 +157,21 @@ def handle_message(event):
         line_bot_api.reply_message(event.reply_token, message)
         return 0
 
-    elif 'art exhibition' in text:
+    if 'art exhibition' in text:
         artfest_content = '[Tech Art Exhibition of 23th NTUArtFest]\nI was the curator of the Tech Art Exhibition.\nI led a team to plan a contemporary dance display that demonstrated a world controlled by digits and people couldn\'t follow their minds.'
         funpark_content = '[10th Very Fun Park]\nI was a guide and an exhibition assistant of Very Fun Park held by Fubonart and National Taiwan University.\nI introduced art works to visitors and helped artists to arrange exhibition and dismantled displayed items.'
         content = '{}\n\n{}'.format(artfest_content, funpark_content)
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=content))
         return 0
 
-    elif 'volunteer club' in text:
+    if 'volunteer club' in text:
         clubs = 'I joined the volunteer club for autism children and the volunteer club for children with cancer.'
         work = 'I worked with people from different backgrounds to plan events like summer camp for those children and their family, which make them relax and connect more with each other.'
         content = '{}\n\n{}'.format(clubs, work)
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=content))
         return 0
 
-    elif 'know more about you' in text:
+    if 'know more about you' in text:
         message = TemplateSendMessage(
             alt_text='Yah, ready to know more?',
             template=ButtonsTemplate(
@@ -193,65 +193,63 @@ def handle_message(event):
         line_bot_api.reply_message(event.reply_token, message)
         return 0
 
-    elif 'motto' in text:
+    if 'motto' in text:
         motto = 'To see the world, things dangerous to come to, to see behind walls, draw closer, to find each other and to feel. That is the purpose of life.'
         sen_content = 'This is my favorite sentence. And I believe technology should have the ability to help people achieve this goal.'
         content = '{}\n\n{}'.format(motto, sen_content)
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=content))
         return 0
 
-    elif 'second language' in text:
+    if 'second language' in text:
         content = 'English: fluent\nKorean: basic'
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=content))
         return 0
 
-    elif 'korean' in text:
+    if 'korean' in text:
         content = '안녕하세요. 저는 오아선 이에요.\nIt means "Hello, I am Wu Ya-Syuan."'
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=content))
         return 0
 
-    elif 'contact' in text or 'communicat' in text or 'phone' in text or 'email' in text:
+    if 'contact' in text or 'communicat' in text or 'phone' in text or 'email' in text:
         content = 'Contact me by:\nEmail: astrumstella048@gmail.com\nPhone: +886 975669869'
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=content))
         return 0
 
-    else:
-        message = TemplateSendMessage(
-            alt_text='Chat with me by...',
-            template=ButtonsTemplate(
-                thumbnail_image_url='https://i.imgur.com/zjN1Pul.jpg',
-                title='Chat with me by...',
-                text='Choose a movie',
-                actions=[
-                    MessageTemplateAction(
-                        label='The Study Diaries',
-                        text='The Study Diaries'
-                    ),
-                    MessageTemplateAction(
-                        label='The Intern',
-                        text='The Intern'
-                    ),
-                    MessageTemplateAction(
-                        label='Project Hunting',
-                        text='Project Hunting'
-                    ),
-                    MessageTemplateAction(
-                        label='School Musical',
-                        text='School Musical'
-                    ),
-                    MessageTemplateAction(
-                        label='Is Not Enough',
-                        text='Is Not Enough'
-                    ),
-                    MessageTemplateAction(
-                        label='Contact',
-                        text='Contact'
-                    ),
-                ]
-            )
+    message = TemplateSendMessage(
+        alt_text='Chat with me by...',
+        template=ButtonsTemplate(
+            thumbnail_image_url='https://i.imgur.com/Hjho3XV.jpg',
+            title='Chat with me by...',
+            text='Choose a movie',
+            actions=[
+                MessageTemplateAction(
+                    label='The Study Diaries',
+                    text='The Study Diaries'
+                ),
+                MessageTemplateAction(
+                    label='The Intern',
+                    text='The Intern'
+                ),
+                MessageTemplateAction(
+                    label='Project Hunting',
+                    text='Project Hunting'
+                ),
+                MessageTemplateAction(
+                    label='School Musical',
+                    text='School Musical'
+                ),
+                MessageTemplateAction(
+                    label='Is Not Enough',
+                    text='Is Not Enough'
+                ),
+                MessageTemplateAction(
+                    label='Contact',
+                    text='Contact'
+                ),
+            ]
         )
-        line_bot_api.reply_message(event.reply_token, message)
-        return 0
+    )
+    line_bot_api.reply_message(event.reply_token, message)
 
 
 if __name__ == "__main__":
